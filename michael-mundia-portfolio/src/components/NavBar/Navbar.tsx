09b2726bc0804a7374046c1315d6101f49f8d1b1
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-15 03:40:15 -0400

    fixed style issues

--- michael-mundia-portfolio/src/components/NavBar/Navbar.tsx
+++ michael-mundia-portfolio/src/components/NavBar/Navbar.tsx
@@ -1,7 +1,6 @@
-import React, { Component, } from 'react';
-// import styled from "styled-components";
-import './Navbar.css';
+import React from 'react';
 
+import './Navbar.css';
 
 export const Navbar = () => {
   return (
