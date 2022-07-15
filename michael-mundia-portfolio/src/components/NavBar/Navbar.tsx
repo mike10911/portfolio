072dc243aac0db7876e090c2b9e739748d77ac71
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-15 03:26:47 -0400

    added navbar

---
+++ michael-mundia-portfolio/src/components/NavBar/Navbar.tsx
@@ -0,0 +1,39 @@
+import React, { Component, } from 'react';
+// import styled from "styled-components";
+import './Navbar.css';
+
+
+export const Navbar = () => {
+  return (
+    <div>
+       <nav className='nav'>
+           <a href='/' className='logo'> MIKE MUNDIA</a>
+           <ul>
+               <li>
+                   <a href='/'>ABOUT</a>
+               </li>
+               <li>
+                   <a href='/'>EXPIRENCE</a>
+               </li>
+               <li>
+                   <a href='/'>SKILLS</a>
+               </li>
+               <li>
+                   <a href='/'>PROJECTS</a>
+               </li>
+               <li>
+                   <a href='/'>THEATRE</a>
+               </li>
+               <li>
+                   <a href='/'>CONTACT</a>
+               </li>
+               <li>
+                   <a href='/'>RESUME</a>
+               </li>
+           </ul>
+       </nav>
+    </div>
+  )
+}
+
+export default Navbar;
