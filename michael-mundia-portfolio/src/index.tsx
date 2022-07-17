a0b19d25ff618cb95139dd6cd9eb026d53bb7e21
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-17 10:00:32 -0400

    fixes

--- michael-mundia-portfolio/src/index.tsx
+++ michael-mundia-portfolio/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Navbar from './components/NavBar/Navbar';
 
 const root = ReactDOM.createRoot(
