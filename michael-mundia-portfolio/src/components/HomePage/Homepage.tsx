a64f3fed3c92c57929e0e2ba17ac0b5cf71a2431
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-18 02:59:47 -0400

    homepage text

---
+++ michael-mundia-portfolio/src/components/HomePage/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Homepage.css';
+
+export const Homepage = () => {
+  return (
+    <div className = 'home'>
+      <h2>HELLO, IM</h2>
+      <h2 className='logo-home'> MIKE MUNDIA</h2>
+      <h2 className ='title-home'> DEVELOPER + ACTOR </h2>
+    </div>
+  )
+}
+
+export default Homepage;
