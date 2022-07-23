414bd9e99c4041e6c94f0f2e43f22763629e3a39
mikeULCRM <104475280+mikeULCRM@users.noreply.github.com>
2022-07-23 17:57:41 -0400

    Homepage -- desktop responsive

--- michael-mundia-portfolio/src/components/HomePage/Homepage.tsx
+++ michael-mundia-portfolio/src/components/HomePage/Homepage.tsx
@@ -2,20 +2,14 @@
 import React from "react";
 import "./Homepage.css";
 import Three from "../common/Three/Three";
-import { AiFillLinkedin, AiFillGithub, } from "react-icons/ai";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import { BsCamera2 } from "react-icons/bs";
 
 export const Homepage = () => {
   return (
-    <section className="landing">
-      <div className="home">
-        <h2>HELLO, IM</h2>
-        <h2 className="logo-home"> mike mundia</h2>
-        <h2 className="title-home"> DEVELOPER + ACTOR + PHOTOGRAPHER</h2>
-        <a href="https://github.com/mike10911" target= '_blank'><AiFillGithub size='3rem'/></a>
-        <a href="https://www.linkedin.com/in/michael-mundia/" target='_blank'><AiFillLinkedin size='3rem'/></a>
-        <a href="https://mikemundia.wordpress.com/" target='_blank'><BsCamera2 size='3rem'/></a>
-      </div>
+    <div className="home">
+      <h2>HELLO, IM</h2>
+      <h2 className="logo-home"> mike mundia</h2>
       <div className="Threes">
         <div className="three1">
           <Three titleName="DEVELOPER" />
@@ -27,7 +21,17 @@ export const Homepage = () => {
           <Three titleName="PHOTGRAPHER" />
         </div>
       </div>
-    </section>
+      <h2 className="title-home"> DEVELOPER + ACTOR + PHOTOGRAPHER</h2>
+      <a href="https://github.com/mike10911" target="_blank">
+        <AiFillGithub size="3rem" />
+      </a>
+      <a href="https://www.linkedin.com/in/michael-mundia/" target="_blank">
+        <AiFillLinkedin size="3rem" />
+      </a>
+      <a href="https://mikemundia.wordpress.com/" target="_blank">
+        <BsCamera2 size="3rem" />
+      </a>
+    </div>
   );
 };
 
